703434029e3ec0ecee4e7e6a15f459a69ccf7176
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2025-07-28 15:45:41 +0100

    fix: order attribute ignored on ItemOderMapper and ItemOderMapper used on OrderMapper

--- ecommerce/src/main/java/com/christian/ecommerce/mapper/ItemOrderMapper.java
+++ ecommerce/src/main/java/com/christian/ecommerce/mapper/ItemOrderMapper.java
@@ -3,6 +3,7 @@ package com.christian.ecommerce.mapper;
 import com.christian.ecommerce.dto.ItemOrderDTO;
 import com.christian.ecommerce.model.ItemOrder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import java.util.List;
@@ -10,11 +11,15 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ItemOrderMapper {
 
+    @Mapping(target = "order", ignore = true)
     ItemOrderDTO itemToDto(ItemOrder item);
 
+    @Mapping(target = "order", ignore = true)
     ItemOrder ItemDtoToItem(ItemOrderDTO itemDTO);
 
+    @Mapping(target = "order", ignore = true)
     List<ItemOrderDTO> itemListToDtoList(List<ItemOrder> items);
 
+    @Mapping(target = "order", ignore = true)
     List<ItemOrder> itemDtoToItemList(List<ItemOrderDTO> itemDTOS);
 }
