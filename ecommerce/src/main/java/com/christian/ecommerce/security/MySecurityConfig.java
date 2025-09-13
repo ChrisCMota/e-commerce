8691a9ad1e89cae8f89a4d7b8abcaa4ce842d08e
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2025-09-13 18:12:06 +0100

    fix: security auth

--- ecommerce/src/main/java/com/christian/ecommerce/security/MySecurityConfig.java
+++ ecommerce/src/main/java/com/christian/ecommerce/security/MySecurityConfig.java
@@ -2,7 +2,6 @@ package com.christian.ecommerce.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,11 +16,23 @@ public class MySecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 
         return httpSecurity.csrf(csrf -> csrf.disable()) //Disables CSRF protection
-                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/products").permitAll() // Allows GET requests to /products without authentication
-                        .requestMatchers(HttpMethod.POST, "/customers").permitAll() // Allows POST requests to /customers without authentication
-                        .requestMatchers(HttpMethod.GET, "/customers").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .anyRequest().authenticated() // Requires authentication for all other requests
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/products").permitAll(); // Allows GET requests to /products without authentication
+                    auth.requestMatchers( "/customers").permitAll(); // Allows POST requests to /customers without authentication
+                    auth.requestMatchers("/customers").permitAll();
+                    auth.requestMatchers( "/login").permitAll();
+                    auth.requestMatchers("/v3/api-docs/**").permitAll();
+                    auth.requestMatchers( "/swagger-ui/**").permitAll();
+                    auth.requestMatchers("/swagger-ui.html").permitAll();
+                    auth.requestMatchers("/swagger-resources/**").permitAll();
+                    auth.requestMatchers("/webjars/**").permitAll();
+                    auth.requestMatchers("/favicon.ico").permitAll();
+                    auth.requestMatchers("/favicon.svg").permitAll();
+
+                    auth.anyRequest().authenticated(); // Requires authentication for all other requests
+                        }
+
                 )
                 .cors(Customizer.withDefaults()) // Enables CORS with default settings (can customize it if needed using: cors -> {} )
                 .addFilterBefore(new MyEComFilter(), UsernamePasswordAuthenticationFilter.class) // Adds a custom filter before the standard username/password filter
